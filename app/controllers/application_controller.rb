0566e69c842b18c3203396e88759ae4120b80314
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-04-10 00:42:29 +0900

    login可能４−１０ 0:42

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  def new
+  before_action :authenticate_user! #log_inしていないユーザーをログインページへ
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :first_name, :last_name, :first_name_prono, :last_name_prono, :birthday])
   end
 end
