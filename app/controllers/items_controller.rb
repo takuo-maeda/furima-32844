d3712b100e5e59eb5ce10157e35ee646176aba65
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-04-14 20:58:40 +0900

    item/New.html.fileへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,7 @@ class ItemsController < ApplicationController
   # end
 
   private
-
-
+  def  item_params
+    params.reqire(:item).permit(:item_name, :description, :category_id, :status_id, :delivery_fee_payment_id, :prefecture_id, :price)
+  end
 end
