b0ca6a381c4ef4d3b774dbcfd34113a20895e23f
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-04-19 20:58:56 +0900

    商品詳細ページ完成４月１９日２１００

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! , except: [:index, :show]
-  # before_action :set_item, only: [:show]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -29,8 +27,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :status_id, :delivery_fee_payment_id, :prefecture_id, :delivery_prepare_id, :price, :image).merge(user_id: current_user.id)
   end
-  
-  # def set_item
-  #   @item = Item.where(user_id: params[:id])
-  # end
 end
