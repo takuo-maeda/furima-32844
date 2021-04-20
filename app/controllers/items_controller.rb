8cd7c877037133dadefe47667c2507a94b2de19e
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-04-20 21:46:52 +0900

    商品編集１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,7 +22,11 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-    
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :status_id, :delivery_fee_payment_id, :prefecture_id, :delivery_prepare_id, :price, :image).merge(user_id: current_user.id)
