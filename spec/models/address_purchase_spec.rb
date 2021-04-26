4d5f818c8a19f99bdaf79df0d7cd92e15bae09d8
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-04-26 21:40:29 +0900

    単体テスト前半完了

--- spec/models/address_purchase_spec.rb
+++ spec/models/address_purchase_spec.rb
@@ -1,3 +1,4 @@
+require 'rails_helper'
 
 RSpec.describe AddressPurchase, type: :model do
   describe '商品購入テスト' do
@@ -13,16 +14,34 @@ RSpec.describe AddressPurchase, type: :model do
       expect(@address_purchase.errors.full_messages).to include("Postal code can't be blank")
     end
     it '郵便番号はハイフンを挟まないと登録できない' do
+        @address_purchase.postal_code = Faker::Lorem.characters(number: 7, min_numeric: 7) 
+        @address_purchase.valid?
+        expect(@address_purchase.errors.full_messages).to include("Postal code は登録できませんでした。ハイフン-(半角)を含みます。")
     end
     it '市町村は空だと登録できない' do
+      @address_purchase.town = ''
+      @address_purchase.valid?
+      expect(@address_purchase.errors.full_messages).to include("Town can't be blank")
     end
     it '市町村は全角ひらがな・全角カタカナ・漢字以外で入力できない' do
+      @address_purchase.town = Faker::Lorem.characters(number: 7, min_alpha: 3, min_numeric: 1) 
+      @address_purchase.valid?
+      expect(@address_purchase.errors.full_messages).to include("Town は全角ひらがな、全角カタカナ、漢字で入力して下さい")
     end
     it '電話番号は空では登録できない' do
+      @address_purchase.phone_number = ''
+      @address_purchase.valid?
+      expect(@address_purchase.errors.full_messages).to include("Phone number can't be blank", "Phone number は半角数値で入力して下さい")
     end
     it '電話番号は半角数値で入力以外で入力できない' do
+      @address_purchase.phone_number = Faker::Lorem.characters(number: 11, min_alpha: 11) 
+      @address_purchase.valid?
+      expect(@address_purchase.errors.full_messages).to include("Phone number は半角数値で入力して下さい")
     end
     it '都道府県はきちんと選択すること' do
+      @address_purchase.prefecture_id = 1
+      @address_purchase.valid?
+      expect(@address_purchase.errors.full_messages).to include("Prefecture must be other than 1")
     end
   end
 end
