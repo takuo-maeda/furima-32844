cd19eb125ad17d7bca221072088f4989c49b68e9
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-05-15 12:45:58 +0900

    preview完成

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
+  if (document.getElementById('item-image')){
     const ImageList = document.getElementById('image-list');
 
     //選択した画像を表示する関数
@@ -8,19 +9,24 @@ document.addEventListener('DOMContentLoaded', function(){
 
       //表示する画像を生成
       const blobImage = document.createElement('img');
-      blobImage.appendChild(imageElement);
+      blobImage.setAttribute('src', blob);
+
+
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
     };
 
     document.getElementById('item-image').addEventListener('change', function(e){
       //画像が表示されている場合のみ、すでに存在している画像を削除する
       const imageContent = document.querySelector('img');
-      if  (imageContent){
+      if (imageContent){
           imageContent.remove();
       }
+      
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
-      createImage(blob);
+      createImageHTML(blob);
     });
   }
 });
